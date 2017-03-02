923915b8f10c28640ab5e44fdae776b5f126466d
mya-mo <miyashuuun0613@gmail.com>
2017-03-02 15:45:05 +0800

    first_fix

--- bbs_no_css.php
+++ bbs_no_css.php
@@ -1,5 +1,44 @@
 <?php
-  // ここにDBに登録する処理を記述する
+$dsn = 'mysql:dbname=oneline_bbs;host=localhost';
+$user = 'root';
+$password = '';
+$dbh = new PDO($dsn,$user,$password);
+$dbh->query('SET NAMES utf8');
+
+if (!empty($_POST)) {
+    // setの方がわかりやすい ?には後でデータを入れる
+    $sql = 'INSERT INTO `posts` SET `id`=NULL,
+                                    `nickname`=?,
+                                    `comment`=?,
+                                    `created`=NOW()
+                                    ';
+    $data = array($_POST['nickname'], $_POST['comment']);
+
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($data); //実行文（?を使う場合は変数名(この場合は$data)をセット
+
+    // ページのリロード処理
+    header('Location: bbs_no_css.php');
+    exit();
+}
+
+$sql = 'SELECT * FROM `posts` ORDER BY `created` DESC';
+// ORDER BY句
+// 指定したカラムの照準(ASC)もしくは降順(DESC)でデータの取得順を指定する(初期設定ではidのASC)
+
+$stmt = $dbh->prepare($sql);
+$stmt->execute();  //stmtにsurverから取得した全件データが入る
+
+// 繰り返し文
+while (true) {  //  条件として無限ループを起こす
+    // $stmtのデータを全件fetchしきたっら繰り返し処理を終了させる
+    $record = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($record === false) {
+        break;
+    }
+    echo $record['nickname'] . ' -' . $record['created'];
+    echo '<br>';
+}
 
 ?>
 <!DOCTYPE html>
